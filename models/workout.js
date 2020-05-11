28ee2e9ed35da3928965701097d7c878d644073c
Kasey Chang <kschang77@yahoo.com>
2020-05-11 15:04:11 -0700

    Model and README

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,7 @@
+// opts toJSON is needed to make sure we pass this
+// virtual field as part of JSON response
+const opts = { toJSON: { virtuals: true } };
+
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
@@ -30,7 +34,17 @@ const workoutSchema = new Schema({
 
     }
   ]
-});
+}, opts);
+
+// create virtual field in Mongoose for totalDuration
+//a field mentioned in workout.js (line 11)
+workoutSchema.virtual('totalDuration').get(function () {
+  // reduce is basically an accumulator, and the
+  //ending ,0 means we start from 0
+  return this.exercises.reduce((ttl, ex) => {
+    return ttl + ex.duration;
+  }, 0)
+})
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
